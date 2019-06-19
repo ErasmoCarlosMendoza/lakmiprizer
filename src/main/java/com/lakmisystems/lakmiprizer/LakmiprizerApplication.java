68e729373972676d5d6568692f83e882fb32029a
ErasmoCarlosMendoza <46898232+ErasmoCarlosMendoza@users.noreply.github.com>
2019-06-19 08:33:40 -0500

    Initial commit

---
+++ src/main/java/com/lakmisystems/lakmiprizer/LakmiprizerApplication.java
@@ -0,0 +1,13 @@
+package com.lakmisystems.lakmiprizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LakmiprizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LakmiprizerApplication.class, args);
+	}
+
+}
